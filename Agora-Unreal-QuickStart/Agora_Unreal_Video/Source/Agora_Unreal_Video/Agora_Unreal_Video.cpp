76b08688f818de67400ae9ea94b057159fe14e82
zhangtao1104 <1831369722@qq.com>
2021-01-25 14:06:27 +0800

    update sdk

--- Agora-Unreal-QuickStart/Agora_Unreal_Video/Source/Agora_Unreal_Video/Agora_Unreal_Video.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Agora_Unreal_Video.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Agora_Unreal_Video, "Agora_Unreal_Video" );
